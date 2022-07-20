6351ea269ee2c87fbd0f07c1f49375009b9b0d11
Kim Coquilla <kimcoquilla@gmail.com>
2022-07-20 19:59:43 -0400

    Created edit update method in Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit destroy]
+
+  def edit; end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path, notice: 'Your bio was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :full_name)
+  end
 end
